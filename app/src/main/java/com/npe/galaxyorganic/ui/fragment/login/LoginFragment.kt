9adeeebcec702a54e537b8ce49718ac597636ef2
renaldysa18 <renaldysab18@gmail.com>
2019-01-15 10:54:47 +0700

    google login version one

--- app/src/main/java/com/npe/galaxyorganic/ui/fragment/login/LoginFragment.kt
+++ app/src/main/java/com/npe/galaxyorganic/ui/fragment/login/LoginFragment.kt
@@ -4,6 +4,7 @@ package com.npe.galaxyorganic.ui.fragment.login
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,22 +13,32 @@ import android.widget.Toast
 import com.facebook.*
 import com.facebook.login.LoginResult
 import com.facebook.login.widget.LoginButton
-
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.npe.galaxyorganic.R
 import com.npe.galaxyorganic.ui.activity.MainActivity
 import kotlinx.android.synthetic.main.fragment_login.view.*
 import org.json.JSONException
-import java.util.*
 import org.json.JSONObject
 import java.net.MalformedURLException
 import java.net.URL
+import java.util.*
 
 
 class LoginFragment : Fragment() {
 
-    private lateinit var callbackManager : CallbackManager
-    private lateinit var btnFacebook : LoginButton
-    private lateinit var btnGoogle : Button
+    private lateinit var callbackManager: CallbackManager
+    private lateinit var btnFacebook: LoginButton
+    private lateinit var btnGoogle: Button
+    private final var GOOGLE_SIGN: Int = 123
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,6 +49,7 @@ class LoginFragment : Fragment() {
 
         btnFacebook = v.btn_login_facebook
         btnGoogle = v.btn_login_google
+        mAuth = FirebaseAuth.getInstance()
 
         callbackManager = CallbackManager.Factory.create()
 
@@ -48,14 +60,15 @@ class LoginFragment : Fragment() {
         btnFacebook.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
             override fun onSuccess(loginResult: LoginResult) {
                 // App code
-                var accessToken : String = loginResult.accessToken.token
+                var accessToken: String = loginResult.accessToken.token
 
-                var request :GraphRequest = GraphRequest.newMeRequest(loginResult.accessToken, object : GraphRequest.GraphJSONObjectCallback{
-                    override fun onCompleted(`object`: JSONObject?, response: GraphResponse?) {
-                        getDataObject(`object`)
-                    }
+                var request: GraphRequest =
+                    GraphRequest.newMeRequest(loginResult.accessToken, object : GraphRequest.GraphJSONObjectCallback {
+                        override fun onCompleted(`object`: JSONObject?, response: GraphResponse?) {
+                            getDataObject(`object`)
+                        }
 
-                })
+                    })
             }
 
             override fun onCancel() {
@@ -67,8 +80,17 @@ class LoginFragment : Fragment() {
             }
         })
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+
+
         btnGoogle.setOnClickListener {
-            loginGoogle()
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, GOOGLE_SIGN)
         }
 
         return v
@@ -76,30 +98,55 @@ class LoginFragment : Fragment() {
 
     private fun getDataObject(jsonObject: JSONObject?) {
         try {
-            var profile_pict : URL = URL("http://graph.facebook.com/"+jsonObject?.getString("id")+"/picture?width=250&height=250")
-        } catch(e : MalformedURLException){
+            var profile_pict: URL =
+                URL("http://graph.facebook.com/" + jsonObject?.getString("id") + "/picture?width=250&height=250")
+        } catch (e: MalformedURLException) {
             e.printStackTrace()
-        } catch(e : JSONException){
+        } catch (e: JSONException) {
             e.printStackTrace()
         }
     }
 
-    private fun loginGoogle() {
-        Toast.makeText(context, "Google", Toast.LENGTH_SHORT).show()
-    }
 
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_SIGN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                var account = task.getResult(ApiException::class.java)
+                if (account != null) {
+                    firebaseAuthWithGoogle(account)
+                }
+            } catch (e: ApiException) {
+                e.printStackTrace()
+            }
+        }
         callbackManager.onActivityResult(requestCode, resultCode, data)
     }
 
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        Log.d("TAG", "firebaseAuthWithGoogle:" + account?.id)
+        val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()){task ->
+                if(task.isSuccessful){
+                    val user = mAuth.currentUser
+                } else {
+                    
+                }
+            }
+    }
+
+
     override fun onResume() {
         super.onResume()
-        if(AccessToken.getCurrentAccessToken() != null && !AccessToken.getCurrentAccessToken().isExpired){
+        if (AccessToken.getCurrentAccessToken() != null && !AccessToken.getCurrentAccessToken().isExpired) {
             Toast.makeText(context, "Login berhasil", Toast.LENGTH_SHORT).show()
             val intent = Intent(activity, MainActivity::class.java)
             startActivity(intent)
         }
     }
+
+
 }
