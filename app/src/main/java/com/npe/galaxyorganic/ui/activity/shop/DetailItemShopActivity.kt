281eade119b76985e7eff6a20b272b3b115fe272
digiponic-renal <renaldy@digiponic.co.id>
2019-01-22 11:25:12 +0700

    button add & minus stock detail item

--- app/src/main/java/com/npe/galaxyorganic/ui/activity/shop/DetailItemShopActivity.kt
+++ app/src/main/java/com/npe/galaxyorganic/ui/activity/shop/DetailItemShopActivity.kt
@@ -3,9 +3,6 @@ package com.npe.galaxyorganic.ui.activity.shop
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.ImageButton
-import android.widget.TextView
-import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.google.gson.Gson
 import com.npe.galaxyorganic.R
@@ -17,6 +14,8 @@ class DetailItemShopActivity : AppCompatActivity() {
     private lateinit var jsonString: String
     private var gson = Gson()
     private lateinit var dataItem : DatumShopItemModel
+    private var stockProduct : Int = 0
+    private var maxStock : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +25,9 @@ class DetailItemShopActivity : AppCompatActivity() {
             jsonString = getExtra.getString("DataItem")
             dataItem = gson.fromJson(jsonString, DatumShopItemModel::class.java)
         }
+
+        maxStock = dataItem.stock!!
+
         Glide.with(applicationContext)
             .load(dataItem.image)
             .into(imgToolbar)
@@ -46,13 +48,30 @@ class DetailItemShopActivity : AppCompatActivity() {
         layout_addToCart_detailItem.visibility = View.VISIBLE
 
         if(layout_addToCart_detailItem.visibility == View.VISIBLE){
+
             btn_addCart_detailItem.setOnClickListener {
-                Toast.makeText(applicationContext, "Add Barang", Toast.LENGTH_SHORT).show()
+                this.stockProduct = this.stockProduct + 1
+                if(this.stockProduct > maxStock){
+                    btn_addCart_detailItem.isClickable = false
+                } else {
+                    displayBuyingStock(this.stockProduct)
+                }
             }
 
             btn_minusCart_detailItem.setOnClickListener {
-                Toast.makeText(applicationContext, "Minus Barang", Toast.LENGTH_SHORT).show()
+                this.stockProduct = this.stockProduct - 1
+                if(this.stockProduct <= 0){
+                    this.stockProduct = 0
+                    layout_addToCart_detailItem.visibility = View.GONE
+                    btn_addToCart_detailItem.visibility = View.VISIBLE
+                } else {
+                   displayBuyingStock(this.stockProduct)
+                }
             }
         }
+    }
+
+    private fun displayBuyingStock(stockProduct: Int) {
+        tv_jumlahBarang_detailItem.text = stockProduct.toString()
     }
 }
