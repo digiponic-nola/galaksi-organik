b00bbde719160aab02c379735a90dd69f769f827
digiponic-renal <renaldy@digiponic.co.id>
2019-01-22 11:04:09 +0700

    layout add to cart

--- app/src/main/java/com/npe/galaxyorganic/ui/activity/shop/DetailItemShopActivity.kt
+++ app/src/main/java/com/npe/galaxyorganic/ui/activity/shop/DetailItemShopActivity.kt
@@ -2,7 +2,10 @@ package com.npe.galaxyorganic.ui.activity.shop
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
+import android.view.View
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.google.gson.Gson
 import com.npe.galaxyorganic.R
@@ -14,6 +17,7 @@ class DetailItemShopActivity : AppCompatActivity() {
     private lateinit var jsonString: String
     private var gson = Gson()
     private lateinit var dataItem : DatumShopItemModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_item_shop)
@@ -30,5 +34,25 @@ class DetailItemShopActivity : AppCompatActivity() {
         tv_namaBarang_detailBarang.text = dataItem.name
         tv_hargaBarang_detailBarang.text = dataItem.sell_price
         tv_desc_detailBarang.text = dataItem.description
+
+
+        btn_addToCart_detailItem.setOnClickListener {
+            showLayoutAddCart()
+        }
+    }
+
+    private fun showLayoutAddCart() {
+        btn_addToCart_detailItem.visibility = View.GONE
+        layout_addToCart_detailItem.visibility = View.VISIBLE
+
+        if(layout_addToCart_detailItem.visibility == View.VISIBLE){
+            btn_addCart_detailItem.setOnClickListener {
+                Toast.makeText(applicationContext, "Add Barang", Toast.LENGTH_SHORT).show()
+            }
+
+            btn_minusCart_detailItem.setOnClickListener {
+                Toast.makeText(applicationContext, "Minus Barang", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
